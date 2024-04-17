bd65b5f0be4dc4d6deb74533507464de85463444
exl7954 <exl7954@nyu.edu>
2024-04-17 04:57:55 -0400

    implement first draft kg visualization

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -2,15 +2,21 @@ import './App.css';
 import { useState } from 'react';
 import SetContext from './components/SetContext';
 import Chat from './components/Chat';
+import KG from './components/KG';
 
 function App() {
   const [context, setContext] = useState("");
+  const [kg, setKG] = useState("");
 
   return (
     <div className="App">
       <SetContext 
         context={context}
         setContext={setContext}
+        setKG={setKG}
+      />
+      <KG 
+        kg={kg}
       />
       <Chat />
     </div>
